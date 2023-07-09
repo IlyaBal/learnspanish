63950d244251d3dc2f61d28bc445664fabb19b97
yevhen bal <yevhen.bal@gmail.com>
2023-07-09 18:52:21 +0200

    te

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter(
       errorElement: <ErrorPage />,
     },
     {
-      path: '/login',
+      path: '/learnspanish',
       element: <SignInSide />,
     },
     {
@@ -33,7 +33,7 @@ const router = createBrowserRouter(
       element: <App />,
     },
   ],
-  { basename: process.env.PUBLIC_URL }
+  { basename: '/learnspanish' }
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
