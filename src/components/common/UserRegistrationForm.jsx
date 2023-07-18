6fa10e0e6912877ee5bbc5a7382005d358fe1776
IlyaBal <ilya.bal.2011@gmail.com>
2023-07-18 12:12:12 +0200

    link fix

--- src/components/common/UserRegistrationForm.jsx
+++ src/components/common/UserRegistrationForm.jsx
@@ -2,8 +2,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -20,8 +19,8 @@ function Copyright(props) {
       {...props}
     >
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" to="/">
+        Learn Spanish
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
