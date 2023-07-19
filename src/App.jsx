ddfced22560bdef71fa44694f67eedfbf7bd08b4
IlyaBal <ilya.bal.2011@gmail.com>
2023-07-19 16:39:04 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ function App() {
   return (
     <>
       <h2>Test!!!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem dolor
+        et veniam inventore explicabo, blanditiis beatae id praesentium maxime
+        expedita reiciendis repellat ipsum, obcaecati minus consectetur,
+        adipisci delectus suscipit voluptates?
+      </p>
     </>
   );
 }
